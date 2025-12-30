8f9a09587f554aea7b64a0978d05f4789efe7dbc
stefhartog <stefhartog@gmail.com>
2025-12-30 16:33:23 +0100

    Cleanup

--- wake_screen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export DISPLAY=:0
-journalctl --user -u lva.service -f | while read line; do
-    if echo "$line" | grep -q "Detected wake word"; then
-        xset dpms force on
-        xset s reset
-    fi
-done
