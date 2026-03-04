51f5250a0e1cf9cb43c1d5b4b3e6304c80b553fe
Omar Amin <65174210+omaramin-2000@users.noreply.github.com>
2026-03-04 16:01:23 +0200

    Add support for wake word sensitivity

--- linux_voice_assistant/entity.py
+++ linux_voice_assistant/entity.py
@@ -8,11 +8,14 @@ from aioesphomeapi.api_pb2 import (  # type: ignore[attr-defined]
     ListEntitiesMediaPlayerResponse,
     ListEntitiesRequest,
     ListEntitiesSwitchResponse,
+    ListEntitiesSelectResponse,
     MediaPlayerCommandRequest,
     MediaPlayerStateResponse,
     SubscribeHomeAssistantStatesRequest,
     SwitchCommandRequest,
     SwitchStateResponse,
+    SelectCommandRequest,
+    SelectStateResponse,    
 )
 from aioesphomeapi.model import (
     EntityCategory,
@@ -36,6 +39,8 @@ SUPPORTED_MEDIA_PLAYER_FEATURES = (
     | MediaPlayerEntityFeature.MEDIA_ANNOUNCE
 )
 
+
+WAKE_WORD_SENSITIVITY_OPTIONS = ["Slightly sensitive", "Moderately sensitive", "Very sensitive"]
 
 class ESPHomeEntity:
     def __init__(self, server: APIServer) -> None:
@@ -358,3 +363,49 @@ class ThinkingSoundEntity(ESPHomeEntity):
             # Always return our internal switch state
             self.sync_with_state()
             yield SwitchStateResponse(key=self.key, state=self._switch_state)
+
+
+class WakeWordSensitivityEntity(ESPHomeEntity):
+    def __init__(
+        self,
+        server: APIServer,
+        key: int,
+        name: str,
+        object_id: str,
+        get_sensitivity: Callable[[], str],
+        set_sensitivity: Callable[[str], None],
+    ) -> None:
+        ESPHomeEntity.__init__(self, server)
+        self.key = key
+        self.name = name
+        self.object_id = object_id
+        self._get_sensitivity = get_sensitivity
+        self._set_sensitivity = set_sensitivity
+        self._state = self._get_sensitivity()
+
+    def sync_with_state(self) -> None:
+        self._state = self._get_sensitivity()
+
+    def update_get_sensitivity(self, get_sensitivity: Callable[[], str]) -> None:
+        self._get_sensitivity = get_sensitivity
+
+    def update_set_sensitivity(self, set_sensitivity: Callable[[str], None]) -> None:
+        self._set_sensitivity = set_sensitivity
+
+    def handle_message(self, msg: message.Message) -> Iterable[message.Message]:
+        if isinstance(msg, SelectCommandRequest) and msg.key == self.key:
+            if msg.state in WAKE_WORD_SENSITIVITY_OPTIONS:
+                self._state = msg.state
+                self._set_sensitivity(msg.state)
+                yield SelectStateResponse(key=self.key, state=self._state, missing_state=False)
+        elif isinstance(msg, ListEntitiesRequest):
+            yield ListEntitiesSelectResponse(
+                object_id=self.object_id,
+                key=self.key,
+                name=self.name,
+                options=WAKE_WORD_SENSITIVITY_OPTIONS,
+                entity_category=EntityCategory.CONFIG,
+            )
+        elif isinstance(msg, SubscribeHomeAssistantStatesRequest):
+            self.sync_with_state()
+            yield SelectStateResponse(key=self.key, state=self._state, missing_state=False)
