5edc549397a1e33c2ee8d314185cc72dbaed4178
Florian Asche <github@florian-asche.de>
2026-04-14 01:13:46 +0200

    fix race condition

--- linux_voice_assistant/satellite.py
+++ linux_voice_assistant/satellite.py
@@ -71,6 +71,19 @@ class VoiceSatelliteProtocol(APIServer):
         super().__init__(state.name)
 
         self.state = state
+        
+        # ✅ Initialisiere ALLE Instanzvariablen zuallererst!
+        self._is_streaming_audio = False
+        self._tts_url: Optional[str] = None
+        self._tts_played = False
+        self._continue_conversation = False
+        self._timer_finished = False
+        self._timer_ring_start: Optional[float] = None
+        self._processing = False
+        self._pipeline_active = False
+        self._external_wake_words: Dict[str, VoiceAssistantExternalWakeWord] = {}
+        
+        # ✅ JETZT erst die Referenz im State setzen - erst ab hier sind wir bereit für Callbacks!
         self.state.satellite = self
         self.state.connected = False
 
@@ -318,16 +331,7 @@ class VoiceSatelliteProtocol(APIServer):
         self.state.mic_volume_entity.update_set_value(lambda val: self.state.persist_mic_volume(float(val)))
 
         # ---- Instance variables ----
-
-        self._is_streaming_audio = False
-        self._tts_url: Optional[str] = None
-        self._tts_played = False
-        self._continue_conversation = False
-        self._timer_finished = False
-        self._timer_ring_start: Optional[float] = None
-        self._processing = False
-        self._pipeline_active = False
-        self._external_wake_words: Dict[str, VoiceAssistantExternalWakeWord] = {}
+        # ✅ Wurden bereits ganz oben initialisiert um Race Condition zu verhindern
         self._disconnect_event = asyncio.Event()
 
     def _set_thinking_sound_enabled(self, new_state: bool) -> None:
