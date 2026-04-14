58fbc10f6da9a319dfbc118df0617dd396112972
Florian Asche <github@florian-asche.de>
2026-04-14 05:24:56 +0200

    lint: fix pylint

--- linux_voice_assistant/__main__.py
+++ linux_voice_assistant/__main__.py
@@ -13,7 +13,7 @@ from typing import List, Optional, Union
 
 import numpy as np
 import soundcard as sc
-from aioesphomeapi.api_pb2 import NumberStateResponse  # type: ignore[attr-defined]
+from aioesphomeapi.api_pb2 import NumberStateResponse  # type: ignore
 from getmac import get_mac_address  # type: ignore
 from pymicro_wakeword import MicroWakeWord, MicroWakeWordFeatures
 from pyopen_wakeword import OpenWakeWord, OpenWakeWordFeatures
